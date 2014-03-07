33fce55d563600694576c64f33225f58b1f9a457
Elmahdi Mahmoud <elmahdi.mahmoud@live.com>
2014-03-07 23:39:07 +0300

    add logo

--- assets/config.rb
+++ assets/config.rb
@@ -9,7 +9,7 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -19,8 +19,11 @@ output_style = :compressed
 line_comments = true
 
 
+
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+asset_cache_buster :none
